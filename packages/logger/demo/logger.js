b1d5f74e60f26868fdf22e9dad6234c2ddc82b60
Avan <layouwen@gmail.com>
2024-02-23 11:07:00 +0800

    chore: rm logger.js

--- packages/logger/demo/logger.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const utils_1 = require("../src/utils");
-const logger = new utils_1.Logger();
-logger.error.error("error");
-logger.daily.warn("warn");
-logger.daily.info("info");
-logger.debug.debug("debug");
